31281566ac0f91abf22fe874852feb1c3386e3f3
andre gomes pinheiro <1727553>
2020-02-11 20:52:08 -0300

    tpgrojeopreg

--- graphic_computation/projects/test/test.cpp
+++ graphic_computation/projects/test/test.cpp
@@ -1,5 +1,8 @@
+#include "./external_libs/GLFW/include/GLFW/glfw3.h"
 #include <iostream>
 using namespace std;
 int main() {
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
     return 0;
 }
